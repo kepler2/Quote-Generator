bbbd128fffae5ab8931c2b9c68ff42ec9d910517
kepler2 <dubfire08@gmail.com>
2020-04-17 22:33:09 +0300

    Added quotes + media queries

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const quoteBtn = document.querySelector('#quoteBtn');
 
 let quotes = [
-    '"The greatest glory in living lies not in never falling, but in rising every time we fall." -Nelson Mandela',
-    '"The way to get started is to quit talking and begin doing." -Walt Disney',
-    '"Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma – which is living with the results of other people\'s thinking." -Steve Jobs',
-    '"If life were predictable it would cease to be life, and be without flavor." -Eleanor Roosevelt',
-    '"If you set your goals ridiculously high and it\'s a failure, you will fail above everyone else\'s success." -James Cameron',
-    '"Life is what happens when you\'re busy making other plans." -John Lennon',
-    '"Tell me and I forget. Teach me and I remember. Involve me and I learn." -Benjamin Franklin',
-    '"It is during our darkest moments that we must focus to see the light." -Aristotle',
-    '"Whoever is happy will make others happy too." -Anne Frank',
-    '"Do not go where the path may lead, go instead where there is no path and leave a trail." -Ralph Waldo Emerson',
-    '"You will face many defeats in life, but never let yourself be defeated." -Maya Angelou',
-    '"In the end, it\'s not the years in your life that count. It\'s the life in your years." -Abraham Lincoln',
-    '"You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose." -Dr. Seuss'
+    '"The greatest glory in living lies not in never falling, but in rising every time we fall." - Nelson Mandela',
+    '"The way to get started is to quit talking and begin doing." - Walt Disney',
+    '"Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma – which is living with the results of other people\'s thinking." - Steve Jobs',
+    '"If life were predictable it would cease to be life, and be without flavor." - Eleanor Roosevelt',
+    '"If you set your goals ridiculously high and it\'s a failure, you will fail above everyone else\'s success." - James Cameron',
+    '"Life is what happens when you\'re busy making other plans." - John Lennon',
+    '"Tell me and I forget. Teach me and I remember. Involve me and I learn." - Benjamin Franklin',
+    '"It is during our darkest moments that we must focus to see the light." - Aristotle',
+    '"Whoever is happy will make others happy too." - Anne Frank',
+    '"Do not go where the path may lead, go instead where there is no path and leave a trail." - Ralph Waldo Emerson',
+    '"You will face many defeats in life, but never let yourself be defeated." - Maya Angelou',
+    '"In the end, it\'s not the years in your life that count. It\'s the life in your years." - Abraham Lincoln',
+    '"You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose." - Dr. Seuss',
+    '"To live is the rarest thing in the world. Most people exist, that is all." – Oscar Wilde',
+    '"I find that the harder I work, the more luck I seem to have." - Thomas Jefferson',
+    '"Success is not final; failure is not fatal: It is the courage to continue that counts." - Winston S. Churchill'
 ];
 
 
@@ -41,5 +44,3 @@ function newQuote() {
     let randomNumber = Math.floor(Math.random() * (quotes.length));
     document.querySelector('#quoteDisplay').innerHTML = quotes[randomNumber];
 }
-
-
